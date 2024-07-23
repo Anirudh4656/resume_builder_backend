2e5ef98159c1f65eaae8edd4e02427ee47f4b7fe
ANIRUDH <anirudh4656@gmail.com>
2024-07-23 07:54:44 +0530

    c

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import { IUser, UserRole } from "./app/schemas/User";
 import authRoutes from "./app/routes/authRoutes";
 import userRoutes from "./app/routes/userRoutes";
 import adminRoutes from "./app/routes/adminRoutes";
+import nodemailer from "nodemailer";
 import { initPassport } from "./app/services/passport-jwt";
 import { roleAuth } from "./app/middlewares/roleAuth";
 import { File } from "./app/schemas/FileSchema";
@@ -25,7 +26,8 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/uploads", express.static("uploads"));
 app.use(bodyParser.json());
-
+app.use(bodyParser.json({ limit: '50mb' })); // Adjust the limit as needed
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 const port = 5000;
 dotenv.config();
 declare global {
@@ -45,7 +47,45 @@ const initApp = async (): Promise<void> => {
   app.get("/", (req: Request, res: Response) => {
     res.send({ status: "ok" });
   });
+  app.post('/api/send-pdf', async (req, res) => {
+    const { pdf, email } = req.body;
+    console.log("Your Target Mail Is - " + email)
+  
+    const transporter = nodemailer.createTransport({
+      host:"smtp.gmail.com",
+      port:587,
+      service: 'gmail',
+      auth: {
+        user: process.env.MAIL_USER,
+  pass: process.env.MAIL_PASS,
+      },
+    });
+  
+    const mailOptions = {
+      // from: 'your-email@gmail.com',
+      to: email,
+      subject: 'Your Resume',
+      text: 'Please find your resume attached.',
+      attachments: [
+        {
+          filename: 'resume.pdf',
+          content: pdf.split('base64,')[1],
+          encoding: 'base64',
+        },
+      ],
+    };
+  
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log("PDF sent to the user via email")
+      res.status(200).send('PDF sent to the user via email');
+    } catch (error) {
 
+      console.log("Dikttt Hogyiii............")
+      console.error('Error sending email', error);
+      res.status(500).send('Error sending email');
+    }
+  });
   app.use("/api", authRoutes);
 
   //to correct middle ware of roleAuth
